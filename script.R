64f53102939063c229f9f39ffb2ca2a33fd7729c
Samuel Provencher <samuel.provencher.1@ulaval.ca>
2020-02-13 11:57:37 -0500

    Ajout de summary

--- script.R
+++ script.R
@@ -1,3 +1,12 @@
 ############ TRAVAIL PRATIQUE
 
 donnees <- read.csv("kc_house_data.csv")
+
+str(donnees)
+summary(donnees)
+
+# ANALYSE EXPLORATOIRE DES DONNEES
+
+library(ggplot2)
+
+ggplot(donnees, aes(x=price, y=bedrooms)) + geom_boxplot()
