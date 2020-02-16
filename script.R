1bf0f69f3d6260f56bf301ccf1c5e93e609000ed
Alyson Marquis <alyson.marquis.2@ulaval.ca>
2020-02-16 14:13:50 -0500

    Prétraitement

--- script.R
+++ script.R
@@ -1,3 +1,26 @@
 ############ TRAVAIL PRATIQUE
 
 donnees <- read.csv("kc_house_data.csv")
+
+str(donnees)
+summary(donnees)
+
+donnees.init <- read.csv("kc_house_data.csv")
+
+donnees <- donnees[!donnees$bathrooms == 0,]
+
+donnees$bedrooms[which(donnees$bedrooms ==33)] <- 3
+
+donnees$yr_renovated[which(donnees$yr_renovated != 0)] <- as.integer(1)
+donnees$yr_renovated[which(donnees$yr_renovated == 0)] <- as.integer(0)
+
+donnees <- donnees[,-1] #enlève ID
+
+donnees$date <- substr(donnees$date,1,8)
+
+annee <- substr(donnees$date,1,nchar(donnees$date)-4)
+mois <- substr(donnees$date,nchar(donnees$date)-3,nchar(donnees$date)-2)
+jour <- substr(donnees$date, nchar(donnees$date)-1, nchar(donnees$date))
+
+donnees$date <- as.POSIXct(paste(annee,mois,jour,sep="-"), format="%Y-%m-%d", tz="UTC")
+
