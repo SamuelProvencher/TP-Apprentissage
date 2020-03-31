aa1b0983ce4ecb647d062495016bc380339a7667
Alexis Picard <alexis.picard.1@ulaval.ca>
2020-03-30 20:10:03 -0400

    Update Bagging_and_RandomForest.R

--- Bagging_and_RandomForest.R
+++ Bagging_and_RandomForest.R
@@ -5,22 +5,32 @@ library(pROC)
 
 #### Bagging ####
 set.seed(69)
-bag <-randomForest(price~., data = donnees.train, proximity=TRUE, ntree=50,importance=TRUE) 
+bag <-randomForest(I(log(price))~., data = donnees.train, proximity=TRUE, ntree=100,importance=TRUE) 
 bag
-bag$mse[]
+bag$mse
 plot(1:length(bag$mse),bag$mse, type="l", xlab="B", ylab="Erreur quadratique moyenne", main="Bagging")
 varImpPlot(bag)
 bag_prev <- predict(bag, newdata=donnees.test, type="response")
-bag.res <- roc(donnees.test$price, bag_prev, auc=TRUE, plot=TRUE)
-bag.res$auc
+#bag.res <- roc(log(donnees.test$price), bag_prev, auc=TRUE, plot=TRUE)
+#bag.res$auc
+EQM.bag <- sum((bag_prev-log(donnees.test$price))^2)/length(donnees.test)
 
 #### Foret aleatoire ####
 oob.values <- vector(length=10)
-for(i in 4:15) {
-    temp.model <- randomForest(price ~ ., data=donnees.train, mtry=i, proximity=TRUE, 
-                               ntree=50,importance=TRUE)
-    oob.values[i] <- temp.model$mse[length(temp.model$mse)]
-}
-oob.values
-#38145767195 21680065279 19448833900 18971641912 18425064898 18093250790 17540099545 17391514525
-# 18313559044 17062334435
+# à ne pas rouler, très long, les résultats sont en bas
+#for(i in 1:15) {
+#    temp.model <- randomForest(I(log(price)) ~ ., data=donnees.train, mtry=i, proximity=TRUE, 
+#                               ntree=50,importance=TRUE)
+#    oob.values[i] <- temp.model$mse[length(temp.model$mse)]
+#}
+# 0.06343711 0.03733935 0.03405029 0.03368466 0.03330120 0.03255624 0.03279187 0.03249364 0.03297875
+#[10] 0.03265353 0.03280722 0.03251453 0.03262090 0.03302811 0.03298384
+# mse minimale à m=8
+foret <-randomForest(I(log(price))~., data = donnees.train, proximity=TRUE, ntree=100,
+                     importance=TRUE, mtry=8) 
+varImpPlot(foret)
+foret_prev <- predict(foret, newdata=donnees.test, type="response")
+#foret.res <- roc(log(donnees.test$price), foret_prev, auc=TRUE, plot=TRUE)
+#bag.res$auc
+EQM.foret <- sum((foret_prev-log(donnees.test$price))^2)/length(donnees.test)
+EQM.foret
