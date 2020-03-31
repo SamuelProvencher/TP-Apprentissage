aca0e1f2915dccfaf8795eec9192086f0fad78df
Alexis Picard <alexis.picard.1@ulaval.ca>
2020-03-31 12:16:12 -0400

    Create 1-ModeleBase.R

---
+++ 1-ModeleBase.R
@@ -0,0 +1,12 @@
+source("script2.R")
+
+ModBase <- lm(I(log(price))~., data = donnees.train)
+summary(ModBase)
+PredBase <- predict(ModBase, newdata = donnees.test, type = "response")
+
+ModBase2 <- lm(I(log(price))~(.)^2, data = donnees.train)
+summary(ModBase2)
+PredBase2 <- predict(ModBase2, newdata = donnees.test, type = "response")
+
+EQM.ModBase <- sum((PredBase-log(donnees.test$price))^2)/length(donnees.test)
+EQM.ModBase2 <- sum((PredBase2-log(donnees.test$price))^2)/length(donnees.test)
