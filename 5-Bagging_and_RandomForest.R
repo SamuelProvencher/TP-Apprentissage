a16aed60ff70f9353859ef227d94c74a25408cf5
Alexis Picard <alexis.picard.1@ulaval.ca>
2020-04-10 18:26:45 -0400

    Update 5-Bagging_and_RandomForest.R

--- 5-Bagging_and_RandomForest.R
+++ 5-Bagging_and_RandomForest.R
@@ -1,7 +1,8 @@
-source("script.R")
+source("script2.R")
 
 library(randomForest)
 library(pROC)
+library(caret)
 
 #### Bagging ####
 set.seed(69)
@@ -14,29 +15,27 @@ set.seed(69)
 plot(1:length(bag$mse),bag$mse, type="l", xlab="B", ylab="Erreur quadratique moyenne", main="Bagging")
 varImpPlot(bag)
 bag_prev <- predict(bag, newdata=donnees.test, type="response")
-#bag.res <- roc(log(donnees.test$price), bag_prev, auc=TRUE, plot=TRUE)
-#bag.res$auc
 (EQM.bag <- mean((bag_prev-log(donnees.test$price))^2))
 
 #### Foret aleatoire ####
-# à ne pas rouler, très long, les résultats sont en bas
-#oob.values <- vector(length=10)
-#for(i in 1:15) {
-#    temp.model <- randomForest(I(log(price)) ~ ., data=donnees.train, mtry=i, proximity=TRUE, 
-#                               ntree=50,importance=TRUE)
-#    oob.values[i] <- temp.model$mse[length(temp.model$mse)]
-#}
-# 0.06343711 0.03733935 0.03405029 0.03368466 0.03330120 0.03255624 0.03279187 0.03249364 0.03297875
-#[10] 0.03265353 0.03280722 0.03251453 0.03262090 0.03302811 0.03298384
-# mse minimale à m=8
+control <- trainControl(method = "cv", number = 5)
+
+rf.train <- train(log(price)~.,
+                  data = donnees.train,
+                  method = "rf",
+                  metric = "RMSE",
+                  tuneGrid = expand.grid(mtry = 2:15),
+                  trControl = control,
+                  sampsize = floor(0.75*nrow(donnees.train)),
+                  ntree = 150,
+                  nodesize=2)
+
 foret <-randomForest(I(log(price))~., data = donnees.train, 
                      sampsize= floor(0.6*nrow(donnees.train)),
                      ntree=100,
                      importance=TRUE, 
-                     mtry=8) 
+                     mtry=9) 
 varImpPlot(foret)
 foret_prev <- predict(foret, newdata=donnees.test, type="response")
-#foret.res <- roc(log(donnees.test$price), foret_prev, auc=TRUE, plot=TRUE)
-#bag.res$auc
 (EQM.foret <- mean((foret_prev-log(donnees.test$price))^2))
 
