55cd4c3f571b7b69b7649b81d5da8aa3eea5f7b4
Samuel Provencher <samuel.provencher.1@ulaval.ca>
2020-04-13 14:26:28 -0400

    Update arbre

--- 4-Arbre.R
+++ 4-Arbre.R
@@ -1,13 +1,54 @@
 #Arbre de décisions
 
-source("script.R")
+source("script2.R")
 
 library(rpart)
 library(rpart.plot)
 
-tree.control <- rpart.control(cp = 0, minbucket = 5)
+##### Optimisation de l'argument minbucket (ou minsplit??? Un des deux je sais pas)
+#### L'optimisation doit se faire manuellement https://stackoverflow.com/questions/36781755/how-to-specify-minbucket-in-caret-train-for?rq=1
+#### Je sais pas si c'est bon
 
-arbre1 <- rpart(I(log(price))~., data=donnees.train, control = tree.control)
+fonction_arbre <- function(x)
+{
+    set.seed(1)
+    
+    tree.control <- rpart.control(cp = 0, minbucket = x) 
+    
+    arbre <- rpart(I(log(price))~., data=donnees.train, control = tree.control, method = "anova")
+    
+    cp.choix <- arbre$cptable[which.min(arbre$cptable[,4]),1]
+    
+    arbre.elague <- prune(arbre, cp = cp.choix)
+    
+    PredArbre <- predict(arbre.elague, type = "vector")
+    
+    EQM.arbre <- sum((PredArbre-log(donnees.train$price))^2)/nrow(donnees.train)
+    
+    data.frame(cp = cp.choix, EQM = EQM.arbre)
+    
+}
+fonction_arbre(x=5)
+
+minbucket <- c(1,5,10, 15, 20, 25, 30, 40, 50, 100, 200)
+
+EQM.test <- sapply(minbucket, function(i) fonction_arbre(x=i)$EQM) #minbucket = 10 amène le plus petit EQM
+
+minbu <- minbucket[which.min(EQM.test)] #10
+
+minbucket2 <- 1:15
+
+EQM.test2 <- sapply(minbucket2, function(i) fonction_arbre(x=i)$EQM)
+
+minbucket.choix <- minbucket2[which.min(EQM.test2)] #9
+
+#### Modèle arbre de régression
+
+set.seed(1)
+
+tree.control <- rpart.control(cp = 0, minbucket = minbucket.choix)
+
+arbre1 <- rpart(I(log(price))~., data=donnees.train, control = tree.control, method = "anova")
 
 plotcp(arbre1) #cross validation en 10 plis
 #On pourrait aussi utiliser le package caret pour avoir le cp optimal, mais ca semble plus complexe
@@ -22,8 +63,12 @@ arbre.elague <- prune(arbre1, cp = cp.choix)
 
 PredArbre <- predict(arbre.elague, newdata = donnees.test, type = "vector") #type=vector pour la régression
 
-rpart.plot(arbre.elague)
+#rpart.plot(arbre.elague) # ne marche pas malheureusement, MP a dit dans le forum que ca se pouvait, car on a beaucoup de données
 
-mean(PredArbre);mean(log(donnees.test$price)) #se ressemble
+#mean(PredArbre);mean(log(donnees.test$price)) #se ressemble
 
-EQM.arbre <- sum((PredArbre-log(donnees.test$price))^2)/nrow(donnees.test)
+(EQM.arbre <- sum((PredArbre-log(donnees.test$price))^2)/nrow(donnees.test)) #0.0477653
+
+
+
+
