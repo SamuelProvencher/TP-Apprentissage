f8fed04f2730105519ce697b5af8be3f45fa66ca
Alexis Picard <alexis.picard.1@ulaval.ca>
2020-04-18 19:00:04 -0400

    Update 5-Bagging_and_RandomForest.R

--- 5-Bagging_and_RandomForest.R
+++ 5-Bagging_and_RandomForest.R
@@ -21,32 +21,33 @@ bag_prev <- predict(bag, newdata=donnees.test, type="response")
 EQM.bag <- mean((bag_prev-log(donnees.test$price))^2)
 
 #### Foret aleatoire ####
-control <- trainControl(method = "cv", number = 5)
-#très long, ne pas rouler
+# control <- trainControl(method = "cv", number = 5)
+# #très long, ne pas rouler
 # rf.train <- train(log(price)~.,
-#                    data = donnees.train,
-#                    method = "rf",
-#                    metric = "RMSE",
-#                    tuneGrid = expand.grid(mtry = 6:12),
-#                    trControl = control,
-#                    sampsize = floor(0.75*nrow(donnees.train)),
-#                    ntree = 150)
-
-#interprétation
- foret.iml <- Predictor$new(rf.train)
- pdp.lat <- FeatureEffect$new(foret.iml, "lat", method = "pdp",
-                                grid.size = 50)
- pdp.long <- FeatureEffect$new(foret.iml, "long", method = "pdp",
-                              grid.size = 50)
- pdp.grade <- FeatureEffect$new(foret.iml, "grade", method = "pdp",
-                              grid.size = 50)
+#                     data = donnees.train,
+#                     method = "rf",
+#                     metric = "RMSE",
+#                     tuneGrid = expand.grid(mtry = 6:12),
+#                     trControl = control,
+#                     sampsize = floor(0.75*nrow(donnees.train)),
+#                     ntree = 150)
+## mtry=10 is the best one
 
 #vrai modèle
  foret <-randomForest(I(log(price))~., data = donnees.train,
                       sampsize= floor(0.75*nrow(donnees.train)),
                       ntree=150,
                       importance=TRUE,
-                      mtry=8)
- importance_var_foret <- varImpPlot(foret)
- foret_prev <- predict(foret, newdata=donnees.test, type="response")
- EQM.foret <- mean((foret_prev-log(donnees.test$price))^2)
+                      mtry=10)
+#interprétation
+foret.iml <- Predictor$new(rf.train)
+pdp.lat <- FeatureEffect$new(foret.iml, "lat", method = "pdp",
+                             grid.size = 50)
+pdp.long <- FeatureEffect$new(foret.iml, "long", method = "pdp",
+                              grid.size = 50)
+pdp.grade <- FeatureEffect$new(foret.iml, "grade", method = "pdp",
+                               grid.size = 50)
+
+importance_var_foret <- varImpPlot(foret)
+foret_prev <- predict(foret, newdata=donnees.test, type="response")
+EQM.foret <- mean((foret_prev-log(donnees.test$price))^2)
